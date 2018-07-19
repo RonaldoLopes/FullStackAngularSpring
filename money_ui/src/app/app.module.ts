1f680b26dd31098216518d7d9f0151e096270dcf
unknown <ronaldorclamaster@gmail.com>
2018-07-19 16:56:26 -0300

    final 17.7

--- money_ui/src/app/app.module.ts
+++ money_ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LancamentoService } from './lancamentos/lancamento.service';
+import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { HttpModule } from '../../node_modules/@angular/http';
+import { PessoaService } from './pessoas/pessoa.service';
+import { LancamentoService } from './lancamentos/lancamento.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,10 @@ import { HttpModule } from '../../node_modules/@angular/http';
     LancamentosModule,
     PessoasModule
   ],
-  providers: [LancamentoService],
+  providers: [
+    LancamentoService,
+    PessoaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- money_ui/src/app/lancamentos/lancamento.service.ts
+++ money_ui/src/app/lancamentos/lancamento.service.ts
@@ -2,9 +2,14 @@ import { Http, Headers, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
+import * as moment from 'moment';
 
-export interface LancamentoFiltro {
-  descricao: string
+export class LancamentoFiltro {
+  descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 
 @Injectable()
@@ -15,19 +20,49 @@ export class LancamentoService {
   constructor(private http: Http) { }
 
   pesquisar(filtro: LancamentoFiltro): Promise<any> {
-
-    const params = new URLSearchParams()
+    const params = new URLSearchParams();
     const headers = new Headers();
 
     headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
+    params.set('page', filtro.pagina.toString());
+    params.set('size', filtro.itensPorPagina.toString());
+
     if (filtro.descricao) {
-      params.set('descricao', filtro.descricao)
+      params.set('descricao', filtro.descricao);
     }
 
-    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, search: filtro })
+    if (filtro.dataVencimentoInicio) {
+      params.set('dataVencimentoDe',
+        moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params.set('dataVencimentoAte',
+        moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'));
+    }
+
+    return this.http.get(`${this.lancamentosUrl}?resumo`,
+        { headers, search: params })
       .toPromise()
-      .then(response => response.json().content)
+      .then(response => {
+        const responseJson = response.json();
+        const lancamentos = responseJson.content;
+
+        const resultado = {
+          lancamentos,
+          total: responseJson.totalElements
+        };
+
+        return resultado;
+      })
   }
+  /*excluir(codigo: number): Promise<void> {
+    const headers = new Headers();
+
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.delete(`${this.lancamentosUrl}/${codigo}`)
+  }*/
 
 }
